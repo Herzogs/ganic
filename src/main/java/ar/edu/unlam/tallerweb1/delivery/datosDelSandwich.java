56ffed6016f49a415197798a5a21d7b3dde97226
Nahuel Gamarra <gamarraorlandonahuel@gmail.com>
2022-09-29 13:56:46 -0300

    creada la clase datosDelSanwich

---
+++ src/main/java/ar/edu/unlam/tallerweb1/delivery/datosDelSandwich.java
@@ -0,0 +1,39 @@
+package ar.edu.unlam.tallerweb1.delivery;
+
+import ar.edu.unlam.tallerweb1.domain.ingredientes.Ingrediente;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class datosDelSandwich {
+    private List<Ingrediente>ingredientesSandwich;
+    private float monto;
+
+    public datosDelSandwich() {
+        this.ingredientesSandwich= new ArrayList<>();
+        this.monto= 0.0F;
+
+    }
+
+    public List<Ingrediente> getIngredientesSandwich() {
+        return ingredientesSandwich;
+    }
+
+    public void setIngredientesSandwich(List<Ingrediente> ingredientesSandwich) {
+        this.ingredientesSandwich = ingredientesSandwich;
+    }
+
+    public float getMonto() {
+        for(Ingrediente ing:ingredientesSandwich){
+            monto+= ing.getPrecio();
+        }
+        return monto;
+    }
+    public void cargarIngredienteAlSandwich(Ingrediente ingrediente){
+        this.ingredientesSandwich.add(ingrediente);
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+}
