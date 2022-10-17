d9774e92d6eae56924336a6ee6feaf071ca77c13
Ivan Ferez <ferezivan@hotmail.com>
2022-10-16 21:56:24 -0300

    Merge fix test

---
+++ src/main/java/ar/edu/unlam/tallerweb1/delivery/ControladorUsuario.java
@@ -0,0 +1,131 @@
+package ar.edu.unlam.tallerweb1.delivery;
+
+import ar.edu.unlam.tallerweb1.domain.Excepciones.LoginInvalidoException;
+import ar.edu.unlam.tallerweb1.domain.Excepciones.UsuarioInvalidoException;
+import ar.edu.unlam.tallerweb1.domain.Excepciones.UsuarioNoRegistradoExepcion;
+import ar.edu.unlam.tallerweb1.domain.usuarios.ServicioLogin;
+import ar.edu.unlam.tallerweb1.domain.usuarios.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ControladorUsuario {
+
+    private ServicioLogin servicioLogin;
+
+    @Autowired
+    public ControladorUsuario(ServicioLogin servicioLogin) {
+        this.servicioLogin = servicioLogin;
+    }
+
+    @RequestMapping("/login")
+    public ModelAndView irALogin() {
+
+        ModelMap modelo = new ModelMap();
+
+        modelo.put("datosLogin", new DatosLogin());
+
+        return new ModelAndView("login", modelo);
+    }
+
+    @RequestMapping(path = "/validar-login", method = RequestMethod.POST)
+    public ModelAndView validarLogin(@ModelAttribute("datosLogin") DatosLogin datosLogin, HttpServletRequest request) {
+        ModelMap model = new ModelMap();
+        try {
+            Usuario usuarioBuscado = servicioLogin.consultarUsuario(datosLogin.getEmail(), datosLogin.getPassword());
+            request.getSession().setAttribute("id", usuarioBuscado.getId());
+            request.getSession().setAttribute("email", usuarioBuscado.getEmail());
+            return new ModelAndView("redirect:/home");
+
+        } catch (LoginInvalidoException e) {
+            model.put("error", e.getMessage());
+            return new ModelAndView("login", model);
+        }
+
+    }
+
+    @RequestMapping(path = "/registrar", method = RequestMethod.GET)
+    public ModelAndView registrarUsuario() {
+        ModelMap model = new ModelMap();
+        model.put("datosLogin", new DatosLogin());
+        return new ModelAndView("registrar", model);
+    }
+
+    @RequestMapping(path = "/crearUsuario", method = RequestMethod.POST)
+    public ModelAndView crearRegistro(DatosLogin datosLogin) {
+        ModelMap model = new ModelMap();
+        if (esValido(datosLogin.getEmail())) {
+            if (!servicioLogin.estaRegistrado(datosLogin.getEmail())) {
+                Usuario usuarioRegistrado = new Usuario(datosLogin.getEmail(), datosLogin.getPassword());
+                model.put("estado", "Registro Exitoso");
+                this.servicioLogin.crearUsuario(usuarioRegistrado);
+                return new ModelAndView("redirect:/login", model);
+            }
+            model.put("msg", "El mail ya se encuentrta registrado");
+            return new ModelAndView("registrar", model);
+        }
+        model.put("msg", "El mail debe ser de formato valido");
+        return new ModelAndView("registrar", model);
+
+    }
+
+    @RequestMapping("/verificar")
+    public ModelAndView verificarDatos(HttpServletRequest request) {
+        Long idLogeado = (Long) request.getSession().getAttribute("id");
+        if (idLogeado != null) {
+            ModelMap modelo = new ModelMap();
+            modelo.put("datosUsuario", new DatosUsuario());
+            return new ModelAndView("verificar", modelo);
+        }
+        return new ModelAndView("redirect:/login");
+
+    }
+
+    @RequestMapping(path = "/verificarDatos", method = RequestMethod.POST)
+    public ModelAndView envioDeVerificacion(DatosUsuario datosUsuario, HttpServletRequest request) {
+        ModelMap model = new ModelMap();
+        Usuario miUsuario = null;
+        Long idLogeado = (Long) request.getSession().getAttribute("id");
+        try {
+            miUsuario = this.servicioLogin.consultarPorID(idLogeado);
+            miUsuario.setNombre(datosUsuario.getNombre());
+            miUsuario.setApellido(datosUsuario.getApellido());
+            miUsuario.setDireccion(datosUsuario.getDireccion());
+            miUsuario.setPreferencia(datosUsuario.getPreferencia());
+            this.servicioLogin.actualizarUsuario(miUsuario);
+            return new ModelAndView("home", model);
+        } catch (UsuarioInvalidoException exc) {
+            model.put("error", exc.getMessage());
+            return new ModelAndView("redirect:/login", model);
+        }
+    }
+
+
+    private boolean esValido(String email) {
+
+        return email.matches("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public ModelAndView inicio() {
+        return new ModelAndView("redirect:/home");
+    }
+
+    @RequestMapping(path = "/nosotros")
+    public ModelAndView nosotros() {
+        return new ModelAndView("nosotros");
+    }
+
+    @RequestMapping(path = "/contacto")
+    public ModelAndView contacto() {
+        return new ModelAndView("contacto");
+    }
+
+}
---
+++ src/test/java/ar/edu/unlam/tallerweb1/delivery/usuario/ControladorUsuarioTest.java
@@ -0,0 +1,183 @@
+package ar.edu.unlam.tallerweb1.delivery.usuario;
+
+import ar.edu.unlam.tallerweb1.delivery.ControladorUsuario;
+import ar.edu.unlam.tallerweb1.delivery.DatosLogin;
+import ar.edu.unlam.tallerweb1.delivery.DatosUsuario;
+import ar.edu.unlam.tallerweb1.domain.Excepciones.UsuarioInvalidoException;
+import ar.edu.unlam.tallerweb1.domain.Excepciones.UsuarioNoRegistradoExepcion;
+import ar.edu.unlam.tallerweb1.domain.usuarios.ServicioLogin;
+import ar.edu.unlam.tallerweb1.domain.usuarios.Usuario;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class ControladorUsuarioTest {
+
+    private ServicioLogin servicioLogin;
+    private ControladorUsuario controladorDeRegistro;
+    private DatosLogin datosLogin;
+
+    private HttpServletRequest request;
+
+    private HttpSession session;
+
+    @Before
+    public void init() {
+        this.servicioLogin = mock(ServicioLogin.class);
+        this.controladorDeRegistro = new ControladorUsuario(this.servicioLogin);
+        this.request = mock(HttpServletRequest.class);
+        this.session = mock(HttpSession.class);
+        when(this.request.getSession()).thenReturn(this.session);
+    }
+
+    @Test
+    public void cuandoSeleccionoElBotonDeRegistarMeEnviaALaVistaRegistrar() {
+        String vistaDestino = "registrar";
+        ModelAndView mod = cuandoLlamoAlControlador();
+        assertThat(mod.getViewName()).isEqualTo(vistaDestino);
+    }
+
+    private ModelAndView cuandoLlamoAlControlador() {
+        ModelAndView mod = this.controladorDeRegistro.registrarUsuario();
+        return mod;
+    }
+
+    @Test
+    public void alIngresarLosDatosSeGuardenEnLaBaseDeDatos()  {
+        String vista_destino ="redirect:/login";
+        this.datosLogin = new DatosLogin();
+        this.datosLogin.setEmail("test@test.com");
+        this.datosLogin.setPassword("123");
+
+        ModelAndView model = this.controladorDeRegistro.crearRegistro(this.datosLogin);
+        assertThat(model.getViewName()).isEqualTo(vista_destino);
+    }
+    @Test
+    public void queNSePuedaGuardarUnUsusrioConMamilYaRegistrado() throws UsuarioNoRegistradoExepcion {
+        String vista_destino ="registrar";
+        DatosLogin nuevoDatosLogin = obtenerUnDatosLogin();
+        ModelAndView model;
+        cuandoUnUsuarioYaExiste("test@test.com");
+        model = this.controladorDeRegistro.crearRegistro(nuevoDatosLogin);
+        assertThat(model.getViewName()).isEqualTo(vista_destino);
+
+    }
+
+    @Test
+    public void queNoMeDejeGuardarUnUsurioConFormatoDeMAilInavlido()  {
+        String vista_destino ="registrar";
+        this.datosLogin = new DatosLogin();
+        this.datosLogin.setEmail("test@test");
+        this.datosLogin.setPassword("123");
+        ModelAndView model = this.controladorDeRegistro.crearRegistro(this.datosLogin);
+        assertThat(model.getViewName()).isEqualTo(vista_destino);
+        assertThat(model.getModel().get("msg")).isEqualTo("El mail debe ser de formato valido");
+    }
+
+    @Test
+    public void cuandoIngresoAVerificarDatosMeDevuelveLaVistaVerificar() {
+        String vistaDestino = "verificar";
+        when((Long) request.getSession().getAttribute("id")).thenReturn(1L);
+        ModelAndView mod = controladorDeRegistro.verificarDatos(request);
+        assertThat(mod.getViewName()).isEqualTo(vistaDestino);
+    }
+
+    // El GetSession pasando el true por parametro, deberia generar una nueva session, pero da null
+    @Test
+    public void cuandoElUsuarioEstaLogeadoYVaAVerificar(){
+        String vistaDestino ="verificar";
+
+        when(this.request.getSession().getAttribute("id")).thenReturn(1L);
+        ModelAndView mod = controladorDeRegistro.verificarDatos(this.request);
+
+        assertThat(mod.getViewName()).isEqualTo(vistaDestino);
+    }
+
+    @Test
+    public void cuandoElUsuarioNoEstaLogeadoYVaAVerificarLoRedirijoALLogin(){
+        String vistaDestino ="redirect:/login";
+
+        when(this.request.getSession().getAttribute("id")).thenReturn(null);
+        ModelAndView mod = controladorDeRegistro.verificarDatos(this.request);
+
+        assertThat(mod.getViewName()).isEqualTo(vistaDestino);
+    }
+
+    @Test
+    public void cuandoElUsuarioEstaLogeadoYRellenoLaVistaVerificacionEnvieAHome() throws UsuarioInvalidoException {
+        DatosUsuario datosUsuario = dadoQueTengoUnosDatosDeUsuario("Juan","Galvez","Direccion","SinRestriccion");
+        Usuario usuario = dadoQueTengoUnUsuarioRegistrado("test@test.com","123");
+        dadoQueTengoUnUsuarioLogeado(1L);
+        cuandoLeDigaAlServicioQueMeBusqueElUsuarioPorID(1L,usuario);
+        ModelAndView model = entoncesLlamoAlEnvioDeVerificacion(datosUsuario,this.request);
+        entoncesVerificoQueMeEnvieALaVistaCorrecta(model,"home");
+    }
+
+    @Test
+    public void cuandoElUsuarioNoEstaLogeadoYRellenoLaVistaVerificacionEnvieALLogin() throws UsuarioInvalidoException {
+        DatosUsuario datosUsuario = dadoQueTengoUnosDatosDeUsuario("Juan","Galvez","Direccion","SinRestriccion");
+        Usuario usuario = dadoQueTengoUnUsuarioRegistrado("test@test.com","123");
+        dadoQueTengoUnUsuarioLogeado(null);
+        cuandoLeDigaAlServicioQueMeBusqueElUsuarioPorIDMeLanzeExcepcion(null);
+        ModelAndView model = entoncesLlamoAlEnvioDeVerificacion(datosUsuario,this.request);
+        entoncesVerificoQueMeEnvieALaVistaCorrecta(model,"redirect:/login");
+    }
+
+    private void cuandoLeDigaAlServicioQueMeBusqueElUsuarioPorIDMeLanzeExcepcion(Long id) throws UsuarioInvalidoException {
+        when(this.servicioLogin.consultarPorID(id)).thenThrow(new UsuarioInvalidoException("No Existe Usuario"));
+    }
+
+    private void entoncesVerificoQueMeEnvieALaVistaCorrecta(ModelAndView model, String vistaEsperada) {
+        assertThat(model.getViewName()).isEqualTo(vistaEsperada);
+    }
+
+    private Usuario dadoQueTengoUnUsuarioRegistrado(String email, String pass) {
+        Usuario us = new Usuario();
+        us.setId(1L);
+        us.setEmail(email);
+        us.setPassword(pass);
+        return us;
+    }
+
+    private void cuandoLeDigaAlServicioQueMeBusqueElUsuarioPorID(Long l, Usuario us) throws UsuarioInvalidoException {
+        when(this.servicioLogin.consultarPorID(l)).thenReturn(us);
+    }
+
+    private ModelAndView entoncesLlamoAlEnvioDeVerificacion(DatosUsuario du,HttpServletRequest request) {
+        return this.controladorDeRegistro.envioDeVerificacion(du,request);
+    }
+
+    private void dadoQueTengoUnUsuarioLogeado(Long l) {
+        when(this.request.getSession().getAttribute("id")).thenReturn(l);
+    }
+
+    private DatosUsuario dadoQueTengoUnosDatosDeUsuario(String nombre , String apellido, String direccion, String pref) {
+        DatosUsuario us = new DatosUsuario();
+        us.setNombre(nombre);
+        us.setApellido(apellido);
+        us.setDireccion(direccion);
+        us.setPreferencia(pref);
+        return us;
+    }
+
+    private DatosLogin obtenerUnDatosLogin() {
+        this.datosLogin = new DatosLogin();
+        this.datosLogin.setEmail("test@test.com");
+        this.datosLogin.setPassword("123");
+        DatosLogin nuevoDatosLogin= datosLogin;
+        return nuevoDatosLogin;
+    }
+    private void cuandoUnUsuarioYaExiste(String mail){
+         when(this.servicioLogin.estaRegistrado(mail)).thenReturn(true);
+    }
+
+}
