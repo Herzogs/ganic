135690e949257c6a82c7c6980e2dbca4dd8f592d
Cristian Feldman <h3rz@telios.io>
2022-11-28 21:25:26 -0300

    change scheduler

--- src/main/java/ar/edu/unlam/tallerweb1/domain/Scheduler/SchedulerTask.java
+++ src/main/java/ar/edu/unlam/tallerweb1/domain/Scheduler/SchedulerTask.java
@@ -34,29 +34,7 @@ public class SchedulerTask {
 
     private static final ServicioEmail servicioEmail = new ServicioEmailImp();
 
-    @Async
-    @Scheduled(cron = "0 * * ? * *", zone = "America/Buenos_Aires")
-    public void EnvioDeEmailCuandoFalten5Minutos() {
-        List<Compra> compraList = null;
-        Boolean envioCorrecto = false;
-        try {
-            compraList = this.servicioCompra.listarComprasPorEstado(EstadoDeCompra.ENCURSO);
-            LocalDateTime actual = LocalDateTime.now(ZoneId.of("America/Buenos_Aires")).withNano(0);
-            Long max_minutes = Long.valueOf(dotenv.get("SCHEDULER_MAX_MINUTES")); //5
-            for (Compra compra: compraList)  {
-                LocalDateTime fechaEntrega = compra.getFechaEntrega();
-                Long minutes = actual.until(fechaEntrega, ChronoUnit.MINUTES);
-                log.info("FECHA DE AVISO: " + fechaEntrega + " , DEL PEDIDO: " + compra.getIdCompra() + " , FECHA ACTUAL: " + actual + " CANTIDAD DE MINUTOS FALTANTES: " + minutes );
-                if ((minutes == max_minutes) || (minutes <= 0) && !envioCorrecto) {
-                    servicioEmail.sendEmail(compra.getUsuario().getEmail(), "Su pedido esta en camino", "En breve estará llegando nuestro repartidor, por favor presta atención y disfruta de tu compra!");
-                    log.info(String.format("Enviando email a %s, sobre la compra %d", compra.getUsuario().getEmail(),compra.getIdCompra()));
-                    envioCorrecto = true;
-                }
-            }
-        } catch (CompraNoEncontradaExeption e) {
-            log.info("No hay entregas en estado pendientes en la base de datos");
-        }
-    }
+
 
     @Async
     @Scheduled(cron = "0 * * ? * *", zone = "America/Buenos_Aires")
@@ -83,9 +61,32 @@ public class SchedulerTask {
 
     @Async
     @Scheduled(cron = "0 * * ? * *", zone = "America/Buenos_Aires")
+    public void EnvioDeEmailCuandoFalten5Minutos() {
+        List<Compra> compraList = null;
+        Boolean envioCorrecto = false;
+        try {
+            compraList = this.servicioCompra.listarComprasPorEstado(EstadoDeCompra.ENCURSO);
+            LocalDateTime actual = LocalDateTime.now(ZoneId.of("America/Buenos_Aires")).withNano(0);
+            Long max_minutes = Long.valueOf(dotenv.get("SCHEDULER_MAX_MINUTES")); //5
+            for (Compra compra: compraList)  {
+                LocalDateTime fechaEntrega = compra.getFechaEntrega();
+                Long minutes = actual.until(fechaEntrega, ChronoUnit.MINUTES);
+                log.info("FECHA DE AVISO: " + fechaEntrega + " , DEL PEDIDO: " + compra.getIdCompra() + " , FECHA ACTUAL: " + actual + " CANTIDAD DE MINUTOS FALTANTES: " + minutes );
+                if ((minutes == max_minutes) || (minutes <= 0) && !envioCorrecto) {
+                    servicioEmail.sendEmail(compra.getUsuario().getEmail(), "Su pedido esta en camino", "En breve estará llegando nuestro repartidor, por favor presta atención y disfruta de tu compra!");
+                    log.info(String.format("Enviando email a %s, sobre la compra %d", compra.getUsuario().getEmail(),compra.getIdCompra()));
+                    envioCorrecto = true;
+                }
+            }
+        } catch (CompraNoEncontradaExeption e) {
+            log.info("No hay entregas en estado pendientes en la base de datos");
+        }
+    }
+
+    @Async
+    @Scheduled(cron = "0 * * ? * *", zone = "America/Buenos_Aires")
     public void actualizarEstadoDeEntregaEnCursoAEntregado() {
         List<Compra> compraList = null;
-        final Integer MINUTO_PARA_PASAR_A_ENCURSO = Integer.valueOf(dotenv.get("MINUTO_PARA_PASAR_A_ENCURSO"));
         try {
             compraList = this.servicioCompra.listarComprasPorEstado(EstadoDeCompra.ENCURSO);
             LocalDateTime actual = LocalDateTime.now(ZoneId.of("America/Buenos_Aires"));
@@ -93,7 +94,7 @@ public class SchedulerTask {
                 LocalDateTime fech1=actual.withNano(0).withSecond(0);
                 LocalDateTime fech2=comp.getFechaEntrega().withNano(0).withSecond(0);
                 log.warn(fech1 + " - " + fech2 + " - " + "minutos: "+ fech2.until(fech1,ChronoUnit.MINUTES));
-                if (fech1.isAfter(fech2)) {
+                if (fech1.until(fech2,ChronoUnit.MINUTES) <= 0) {
                     this.servicioCompra.entregarCompra(comp.getIdCompra());
                     log.info("ENTREGANDO EL PEDIDO: " + comp.getIdCompra());
                 }
